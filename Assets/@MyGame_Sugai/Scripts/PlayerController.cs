e30c30964c560c857faeed2014d4695f5dd4ad08
須貝萌 <ms.march.1234@gmail.com>
2016-03-21 15:52:25 +0900

    FirstCommit

---
+++ Assets/@MyGame_Sugai/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+	private float moveHorizontal;
+	private float moveVertical;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update () {
+		// Input系はUpdateで
+		moveHorizontal = Input.GetAxis("Horizontal");
+		moveVertical = Input.GetAxis("Vertical");
+	}
+	void FixedUpdate()
+	{
+		// 実際に動かすのはFixedUpdateで
+		Vector3 movement = new Vector3( moveHorizontal, 0.0f, moveVertical );
+
+		rb.AddForce(movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Item")) {
+			other.gameObject.SetActive (false);
+		}
+	}
+}
